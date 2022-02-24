14fc855c3e0dd7f320bf9ec2a559da4fa45542d3
Leonardo-Zhou <2974519865@qq.com>
2022-02-24 14:54:36 +0800

    Initial commit

---
+++ download_sub_and_video.js
@@ -0,0 +1,166 @@
+// ==UserScript==
+// @name YouTube 字幕视频下载
+// @name:pt-BR download subtitle and video
+// @name:ar download subtitle and video
+// @name:bg download subtitle and video
+// @name:cs download subtitle and video
+// @name:da download subtitle and video
+// @name:de download subtitle and video
+// @name:el download subtitle and video
+// @name:eo download subtitle and video
+// @name:es download subtitle and video
+// @name:fi download subtitle and video
+// @name:fr download subtitle and video
+// @name:fr-CA download subtitle and video
+// @name:he download subtitle and video
+// @name:hu download subtitle and video
+// @name:id download subtitle and video
+// @name:it download subtitle and video
+// @name:ja download subtitle and video
+// @name:ko download subtitle and video
+// @name:nb download subtitle and video
+// @name:nl download subtitle and video
+// @name:pl download subtitle and video
+// @name:ro download subtitle and video
+// @name:ru download subtitle and video
+// @name:sk download subtitle and video
+// @name:sr download subtitle and video
+// @name:sv download subtitle and video
+// @name:th download subtitle and video
+// @name:tr download subtitle and video
+// @name:uk download subtitle and video
+// @name:ug download subtitle and video
+// @name:vi download subtitle and video
+// @name:zh-CN YouTube 字幕视频下载
+// @name:zh-TW YouTube 字幕视频下载
+// @description Compatible with YouTube MP3 Downloader! This script helps to add a download button more quickly to download videos and audios from YouTube.
+// @description:pt-BR Compatível com o YouTube MP3 Downloader! Esse script ajuda a adicionar um botão de download mais rapidamente para baixar vídeos e áudios do YouTube.
+// @description:ar متوافق مع يوتيوب MP3 تنزيل! يساعد هذا البرنامج النصي على إضافة زر تنزيل بشكل أسرع لتنزيل مقاطع الفيديو والتسجيلات الصوتية من YouTube.
+// @description:bg Съвместим с MP3 Downloader на YouTube! Този скрипт помага да добавите по-бързо бутон за изтегляне, за да изтеглите видеоклипове и аудио от YouTube.
+// @description:cs Kompatibilní s YouTube MP3 Downloader! Tento skript pomáhá rychleji přidat tlačítko pro stahování videí a audia z YouTube.
+// @description:da Kompatibel med YouTube MP3 Downloader! Dette script hjælper med at tilføje en download-knap hurtigere for at downloade videoer og audios fra YouTube.
+// @description:de Kompatibel mit YouTube MP3 Downloader! Mit diesem Skript können Sie schneller eine Download-Schaltfläche hinzufügen, um Videos und Audios von YouTube herunterzuladen.
+// @description:el Συμβατό με το YouTube MP3 Downloader! Αυτό το σενάριο βοηθά να προσθέσετε ένα κουμπί λήψης πιο γρήγορα για να κατεβάσετε βίντεο και ήχους από το YouTube.
+// @description:eo Kongrua kun YouTube MP3 Downloader! Ĉi tiu skripto helpas aldoni elŝutan butonon pli rapide por elŝuti filmetojn kaj sonojn de YouTube.
+// @description:es Compatible con YouTube MP3 Downloader! Este script ayuda a agregar un botón de descarga más rápidamente para descargar videos y audios de YouTube.
+// @description:fi Yhteensopiva YouTube MP3 Downloader -sovelluksen kanssa! Tämä skripti auttaa lisäämään latauspainikkeen nopeammin videoiden ja äänien lataamiseen YouTubesta.
+// @description:fr Compatible avec YouTube MP3 Downloader! Ce script permet d'ajouter un bouton de téléchargement plus rapidement pour télécharger des vidéos et des fichiers audio à partir de YouTube.
+// @description:fr-CA Compatible avec YouTube MP3 Downloader! Ce script permet d'ajouter un bouton de téléchargement plus rapidement pour télécharger des vidéos et des fichiers audio à partir de YouTube.
+// @description:he תואם להורדת MP3 של YouTube! סקריפט זה עוזר להוסיף לחצן הורדה במהירות רבה יותר להורדת סרטונים ושמעים מ- YouTube.
+// @description:hu Kompatibilis a YouTube MP3 Downloader-rel! Ez a szkript segít a letöltési gomb gyorsabb hozzáadásában a videók és audiók letöltéséhez a YouTube-ról.
+// @description:id Kompatibel dengan YouTube MP3 Downloader! Skrip ini membantu menambahkan tombol unduhan lebih cepat untuk mengunduh video dan audio dari YouTube.
+// @description:it Compatibile con YouTube MP3 Downloader! Questo script aiuta ad aggiungere un pulsante di download più rapidamente per scaricare video e audio da YouTube.
+// @description:ja YouTube MP3ダウンローダーと互換性があります！このスクリプトは、YouTubeからビデオやオーディオをダウンロードするためのダウンロードボタンをより迅速に追加するのに役立ちます。
+// @description:ko YouTube MP3 다운로더와 호환! 이 스크립트는 YouTube에서 비디오 및 오디오를 다운로드하기 위해 다운로드 버튼을 더 빨리 추가하는 데 도움이됩니다.
+// @description:nb Kompatibel med YouTube MP3 Downloader! Dette skriptet hjelper deg med å legge til en nedlastningsknapp raskere for å laste ned videoer og lydbånd fra YouTube.
+// @description:nl Compatibel met YouTube MP3 Downloader! Dit script helpt om een downloadknop sneller toe te voegen om video's en audio van YouTube te downloaden.
+// @description:pl Kompatybilny z YouTube MP3 Downloader! Ten skrypt pomaga szybciej dodać przycisk pobierania, aby pobierać filmy i pliki audio z YouTube.
+// @description:ro Compatibil cu YouTube MP3 Downloader! Acest script vă ajută să adăugați mai rapid un buton de descărcare pentru a descărca videoclipuri și audio de pe YouTube.
+// @description:ru Совместимо с YouTube MP3 Downloader! Этот скрипт помогает быстрее добавить кнопку загрузки для загрузки видео и аудио с YouTube.
+// @description:sk Kompatibilné s aplikáciou YouTube na stiahnutie MP3! Tento skript pomáha rýchlejšie pridávať tlačidlo na sťahovanie videí a audia z YouTube.
+// @description:sr Kompatibilan s YouTube MP3 Downloader-om! Ova skripta pomaže bržem dodavanju gumba za preuzimanje za preuzimanje videozapisa i audio zapisa s YouTubea.
+// @description:sv Kompatibel med YouTube MP3 Downloader! Detta skript hjälper till att lägga till en nedladdningsknapp snabbare för att ladda ner videor och ljud från YouTube.
+// @description:th เข้ากันได้กับ YouTube MP3 Downloader! สคริปต์นี้ช่วยเพิ่มปุ่มดาวน์โหลดได้เร็วขึ้นเพื่อดาวน์โหลดวิดีโอและไฟล์เสียงจาก YouTube
+// @description:tr YouTube MP3 Downloader ile uyumlu! Bu komut dosyası, YouTube'dan video ve ses indirmek için bir indirme düğmesi daha hızlı eklemenize yardımcı olur.
+// @description:uk Сумісний з YouTube MP3 Downloader! Цей сценарій допомагає швидше додати кнопку завантаження для завантаження відео та аудіо з YouTube.
+// @description:ug YouTube MP3 Downloader بىلەن ماسلىشالايدۇ! بۇ قوليازما YouTube دىن سىن ۋە ئۈنئالغۇنى چۈشۈرۈش ئۈچۈن چۈشۈرۈش كۇنۇپكىسىنى تېزرەك قوشۇشقا ياردەم بېرىدۇ.
+// @description:vi Tương thích với YouTube MP3 Downloader! Kịch bản này giúp thêm nút tải xuống nhanh hơn để tải xuống video và âm thanh từ YouTube.
+// @description:zh-CN 与YouTube MP3下载器兼容！该脚本有助于更快地添加下载按钮，以从YouTube下载视频和音频。
+// @description:zh-TW 與YouTube MP3下載器兼容！該腳本有助於更快地添加下載按鈕，以從YouTube下載視頻和音頻。
+// @namespace https://greasyfork.org/users/152924
+// @homepageURL https://greasyfork.org/scripts/34613
+// @supportURL https://greasyfork.org/scripts/34613/feedback
+// @author Punisher
+// @version 7.6
+// @date 2021-10-24
+// @icon https://i.imgur.com/InuDDVK.png
+// @compatible chrome
+// @compatible firefox
+// @compatible opera
+// @compatible safari
+// @compatible edge
+// @antifeature referral-link
+// @license CC-BY-NC-ND-4.0
+// @match *://*.youtube.com/*
+// ==/UserScript==
+
+(function() {
+    if (document.getElementById("browser-app") || document.getElementById("masthead") || window.Polymer) {
+        setInterval(function() {
+            if (window.location.href.indexOf("watch?v=") < 0) {
+                return false;
+            }
+            if (document.getElementById("meta-contents") && document.getElementById("punisher") === null) {
+                AddYT();
+            }
+        }, 1);
+
+        setElement = function(url) {
+            var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+            var match = String(url).match(regExp);
+            return (match&&match[7].length==11)? match[7]: false;
+        };
+    }
+
+    function ParseURL(url) {
+        return url.replace(/:/g,"%253A").replace(/\//g,"%252F").replace(/=/g,"%253D").replace(/&/g,"%2526").replace(/\?/g,"%253F");
+    }
+
+    function AddYT() {
+        var buttonDiv = document.createElement("span");
+        buttonDiv.id = "punisher";
+        buttonDiv.style.width = "100%";
+        buttonDiv.style.marginTop = "3px";
+        buttonDiv.style.padding = "10px 0";
+        var addButton = document.createElement("a");
+        addButton.appendChild(document.createTextNode("VIDEO"));
+        addButton.style.width = "50%";
+        addButton.style.cursor = "pointer";
+        addButton.style.height = "inherit";
+        addButton.style.backgroundColor = "#393939";
+        addButton.style.color = "#ffffff";
+        addButton.style.padding = "10px 22px";
+        addButton.style.margin = "0px 0px";
+        addButton.style.border = "0";
+        addButton.style.borderRadius = "2px";
+        addButton.style.fontSize = "1.4rem";
+        addButton.style.fontFamily = "inherit";
+        addButton.style.textAlign = "center";
+        addButton.style.textDecoration = "none";
+        addButton.href = "//yt1s.com/en?q=" + encodeURIComponent(location.href);
+        addButton.target = "_blank";
+        var subButton = document.createElement("a");
+        subButton.appendChild(document.createTextNode("SUBTITLE"));
+        subButton.style.width = "50%";
+        subButton.style.height = "inherit";
+        subButton.style.cursor = "pointer";
+        subButton.style.backgroundColor = "#F4A460";
+        subButton.style.color = "#ffffff";
+        subButton.style.padding = "10px 22px";
+        subButton.style.margin = "0px 0px";
+        subButton.style.border = "0";
+        subButton.style.borderRadius = "2px";
+        subButton.style.fontSize = "1.4rem";
+        subButton.style.fontFamily = "inherit";
+        subButton.style.textAlign = "center";
+        subButton.style.textDecoration = "none";
+        subButton.href = "//youtube4kdownloader.com/download/subtitles/" + ParseURL(location.href);
+        console.log(ParseURL(location.href));
+        console.log(typeof location.href);
+        subButton.target = "_blank";
+        buttonDiv.appendChild(addButton);
+        buttonDiv.appendChild(subButton);
+        var targetElement = document.querySelectorAll("[id='subscribe-button']");
+        if(targetElement){
+            for(var i = 0; i < targetElement.length; i++){
+                if(targetElement[i].className.indexOf("ytd-video-secondary-info-renderer") > -1){
+                    targetElement[i].appendChild(buttonDiv);
+                }
+            }
+        }
+        var descriptionBox = document.querySelectorAll("ytd-video-secondary-info-renderer");
+        if(descriptionBox[0].className.indexOf("loading") > -1){
+            descriptionBox[0].classList.remove("loading");
+        }
+    }
+})();
